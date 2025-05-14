93dbd9e8e6f0293b327662f0d5786c5b6d4f0b21
German <fedosihingerman@yandex.ru>
2025-05-14 03:04:00 +0300

    Add local-storage logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState(() => {
+    return localStorage.getItem("state") || 0;
+  });
 
-  const [state, setState] = useState(0);
+  useEffect(() => {
+    localStorage.setItem("state", state);
+  }, [state]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Счетчик: {state}</h1>
-        <button onClick={()=>setState(state + 1)}>Добавить</button>
-        <button onClick={()=>setState(state - 1)}>Уменьшить</button>
+        <button onClick={() => setState(state + 1)}>Добавить</button>
+        <button onClick={() => setState(state - 1)}>Уменьшить</button>
       </header>
     </div>
   );
